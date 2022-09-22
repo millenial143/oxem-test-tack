0c4dd8bbefeab400621adf76c1bbe455319fd57a
Михаил Беспалов <amut.besp@gmail.com>
2022-09-22 17:21:03 +0500

    - добавлен класс Show для вывода информации в консоль, методы вывода перенесены из Farm в Show - реализована автозагрузка классов по psr-4 при помощи composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '506034e470c6ffd371abf6f2c557aa0ceade60af',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '506034e470c6ffd371abf6f2c557aa0ceade60af',
+            'dev_requirement' => false,
+        ),
+    ),
+);
